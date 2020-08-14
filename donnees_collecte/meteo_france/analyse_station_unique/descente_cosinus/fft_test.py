2157c7814defed5f4f50f41dea5e41db816971e9
Helene <helene.siboulet.84@gmail.com>
2020-08-14 17:28:38 +0200

    reprise fft : temperature + humidite
    Signed-off-by: Helene <helene.siboulet.84@gmail.com>

--- donnees_collecte/meteo_france/analyse_station_unique/descente_cosinus/fft_test.py
+++ donnees_collecte/meteo_france/analyse_station_unique/descente_cosinus/fft_test.py
@@ -15,7 +15,7 @@ for annee in temperature[1].keys() :
 	humi = humi + humidite[1][annee]
 
 
-Y = np.fft.fft(temp - np.mean(temp))
+Y = np.fft.fft(temp - np.mean(temp) + 1)
 freq = np.fft.fftfreq(len(temp), d = temperature[0]["1996"][1] - temperature[0]["1996"][0])
 
 lY = np.log(Y)
@@ -26,13 +26,36 @@ print(Y[f])
 
 plt.plot(freq,lY)
 
-plt.savefig("fft.png")
+plt.savefig("fftT.png")
 
-with open("fft.csv",'w') as csvfile :
+with open("fftT.csv",'w') as csvfile :
 	texte = ""
 	texte = texte + '"frequence";"amplitude";"logAmplitude";"amplitudePhase";"logAmplitudePhase"\n'
 	for k in range(len(freq)) :
 		texte = texte + f"{freq[k]};{np.real(Y[k])};{np.real(lY[k])};{np.imag(Y[k])};{np.imag(lY[k])}\n"
+	texte = texte.replace(".",",")
+	csvfile.write(texte)
+
+plt.clf ()
+
+Y2 = np.fft.fft(humi - np.mean(humi) + 1)
+freq2 = np.fft.fftfreq(len(humi), d = humidite[0]["1996"][1] - humidite[0]["1996"][0])
+
+lY2 = np.log(Y2)
+maxY2 = max(lY2)
+f2 = list(lY2).index(maxY2)
+print(1/freq2[f])
+print(Y2[f])
+
+plt.plot(humi,lY2)
+
+plt.savefig("fftH.png")
+
+with open("fftH.csv",'w') as csvfile :
+	texte = ""
+	texte = texte + '"frequence";"amplitude";"logAmplitude";"amplitudePhase";"logAmplitudePhase"\n'
+	for k in range(len(freq2)) :
+		texte = texte + f"{freq2[k]};{np.real(Y2[k])};{np.real(lY2[k])};{np.imag(Y2[k])};{np.imag(lY2[k])}\n"
 	texte = texte.replace(".",",")
 	csvfile.write(texte)
 
