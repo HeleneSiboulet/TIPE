7e01c617121e0896ba78f8e894601f28d19ddbad
Helene <helene.siboulet.84@gmail.com>
2020-08-15 20:55:01 +0200

    Courbe ecart type persistance
    Signed-off-by: Helene <helene.siboulet.84@gmail.com>

--- donnees_collecte/meteo_france/analyse_station_unique/persistance/persistance.py
+++ donnees_collecte/meteo_france/analyse_station_unique/persistance/persistance.py
@@ -1,5 +1,6 @@
 import json
 import numpy as np
+from matplotlib import pyplot as plt
 
 with open('../temperatureTest.json') as jsonfile :
 	temperature = json.load(jsonfile)
@@ -15,7 +16,7 @@ j = []
 t = []
 h = []
 
-for i in range (10) :
+for i in range (1,10) :
 	j.append(i)
 	for annee in temperature[0].keys () :
 		for date in temperature[0][annee] :
@@ -40,3 +41,10 @@ for i in range (10) :
 print ("j = " + str (j))
 print ("t = " + str (t))
 print ("h = " + str (h))
+
+plt.plot(j,t)
+plt.savefig("Tpersistance_EC.png")
+
+plt.clf()
+plt.plot(j,h)
+plt.savefig("Hpersisance_EC.png")
