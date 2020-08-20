5ed9c3c3c0cb197493d488f8af7054898a3c31ea
Helene <helene.siboulet.84@gmail.com>
2020-08-20 10:46:34 +0200

    ajout de commentaires dans IA
    Signed-off-by: Helene <helene.siboulet.84@gmail.com>

--- IA/run.py
+++ IA/run.py
@@ -1,5 +1,4 @@
 import json
-from sklearn.preprocessing import StandardScaler
 import torch
 import numpy as np
 
@@ -25,12 +24,12 @@ temp = []
 for i in temperature[0].keys() :
 	for j in range(len(temperature[0][i])) :
 		annee.append(float(i))
-		temp.append(temperature[1][i][j])
+		temp.append(temperature[1][i][j])  #enlever les clés
 	
 annee = torch.tensor(annee)
 temp = torch.tensor(temp)
 ma = float(torch.mean(annee))
-sta = float(torch.std(annee))
+sta = float(torch.std(annee)) #ecart type
 mt = float(torch.mean(temp))
 stt = float(torch.std(temp))
 for i in temperature[0].keys() :
@@ -54,10 +53,10 @@ import random
 class Net(nn.Module) :
 
     def __init__(self) :
-        super(Net, self).__init__()
-        self.fc1 = nn.Linear(224, 56).double()
+        super(Net, self).__init__()            #appel init de nn module
+        self.fc1 = nn.Linear(224, 56).double() #ajout de fcc1 à la ft.   224 valeurs (annee, sin, cos, temp) en une semaine, 56 mesure
         self.fc2 = nn.Linear(56, 56).double()
-        self.fc3 = nn.Linear(56, 8).double()
+        self.fc3 = nn.Linear(56, 8).double()  #sortie : temp d'une journee
         self.prelu1 = nn.PReLU().double()
         self.prelu2 = nn.PReLU().double()
 
@@ -71,33 +70,33 @@ class Net(nn.Module) :
 #print('\n\n\n')
 
 net = Net()
-criterion = nn.MSELoss()
+criterion = nn.MSELoss()	#critère de distance
 optimizer = optim.Adam(net.parameters(), lr=10**(-3))
 print(net)
-index_test = []
+index_test = []	#liste d'indice d'ordre aléatoire
 for i in range(longueur, len(temperature_continu) - 8) :
 	index_test.append(i)
 
 for tour in range(1000) :
-	optimizer.zero_grad()
+	optimizer.zero_grad()	#initialise pts de départ du gradient pour ce tour
 	batch_source = []
 	batch_target = []
 	for j in range(20) :
 		i = random.choice(index_test)
-		batch_source.append(torch.tensor(temperature_continu[i-longueur:i]).double().view(1,-1))
+		batch_source.append(torch.tensor(temperature_continu[i-longueur:i]).double().view(1,-1))	#view : mettre ds une matrie de "1" colonne
 		target = []
 		for k in range(i, i+8) :
 			target.append(temperature_continu[k][3])
-		batch_target.append(torch.tensor(target).double().unsqueeze(0))
-	source = torch.cat(batch_source,0)
+		batch_target.append(torch.tensor(target).double().unsqueeze(0)) #unsqueeze rajoute une dimension
+	source = torch.cat(batch_source,0)	#cat : concaténation de tenseurs colonne -> grille
 	target = torch.cat(batch_target,0)
 
 	out = net(source)
 	#print(out)
 	#print(target)
-	loss = criterion(out,target)
+	loss = criterion(out,target)	#criterion renvoie les pertes et le gradient
 	#print(loss)
-	loss.backward()
+	loss.backward()		#rétropropagation
 	optimizer.step()
 	if tour % 100 == 0 :
 		print(tour/100000)
@@ -116,7 +115,7 @@ for i in range(0,taille,8) :
 	sortie = []
 	for j in range(56) :
 		an = (2019.0 - ma)/sta
-		cdt,sdt = convertiseur_date(temperature_test[0]["2019"][(j+i)%taille])
+		cdt,sdt = convertiseur_date(temperature_test[0]["2019"][(j+i)%taille])	#à coller avec début 2020
 		tp = (temperature_test[1]["2019"][(j+i)%taille] - mt)/stt
 		entre.append([an,cdt,sdt,tp])
 	for j in range(8) :
