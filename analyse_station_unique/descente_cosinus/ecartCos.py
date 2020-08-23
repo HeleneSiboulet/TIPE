4b06ea20c7e2196d29870008b86780f9a6b3ece1
Helene <helene.siboulet.84@gmail.com>
2020-08-23 17:51:25 +0200

    Rapport
    Signed-off-by: Helene <helene.siboulet.84@gmail.com>

--- analyse_station_unique/descente_cosinus/ecartCos.py
+++ analyse_station_unique/descente_cosinus/ecartCos.py
@@ -8,7 +8,6 @@ with open('../json/humiditeTest.json') as jsonfile :
 	humidite = json.load(jsonfile)
 
 
-
 ecartf = 0
 ecartg = 0
 ecarth = 0
@@ -31,3 +30,29 @@ print ("T")
 print("ecart f : " + str(ef) + "°C")
 print("ecart g : " + str(eg) + "°C")
 print("ecart h : " + str(eh) + "°C")
+
+
+
+ecartf = 0
+ecartg = 0
+ecarth = 0
+compt = 0
+for annee in humidite[0].keys() :
+	for date in humidite[0][annee] :
+		mesure = humidite[1][annee][humidite[0][annee].index(date)]
+		f =	12.73536399*np.cos(2*np.pi*date + 5.61376937) + 6.78906587*np.cos(2*np.pi*date/365 + 0.31944989) + 71.15758689
+		g = 12.90397869*np.cos(2*np.pi*date + 5.62646828) -2.70175487 *np.cos(4*np.pi*date + 5.69268334) + 71.15503638 + 6.87475093*np.cos(2*np.pi*date/365 + 0.31901173) + 2.40357027*np.cos(4*np.pi*date/365 + 0.89433251)
+		h = 12.95354558*np.cos(2*np.pi*date + 5.62648561 ) - 2.72510888 *np.cos(4*np.pi*date + 5.66712864) + -0.16110029 *np.cos(4*np.pi*date + 6.21532372) + 71.1553131 +6.90021219*np.cos(2*np.pi*date/365 + 0.3193087 ) + 2.50198275*np.cos(4*np.pi*date/365 + 1.00739916) + 1.38720613*np.cos(4*np.pi*date/365 + 5.39421813)
+		ecartf += (f - mesure)**2
+		ecartg += (g - mesure)**2
+		ecarth += (h - mesure)**2
+		compt += 1
+
+ef = (ecartf/compt)** (1/2)
+eg = (ecartg/compt)** (1/2)
+eh = (ecarth/compt)** (1/2)
+print (" ")
+print ("H")
+print("ecart f : " + str(ef) + "%")
+print("ecart g : " + str(eg) + "%")
+print("ecart h : " + str(eh) + "%")
